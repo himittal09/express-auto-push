f7f147ef021c44523cb622faa13301be003888e2
Himanshu Mittal <mittal01091997@gmail.com>
2019-06-13 00:38:05 +0530

    Added option to supply static path to asset folder

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-this is a package
 
-#How to use
+# Express Auto Push
+
+## Points About the package
+
+* You need to use the express's cookieparrser middleware before this package for it to work correctly
+* The path to the static assets folder can be relative or absolute (static)
+* If relative path is supplied, then the flag `isPathStatic` must be `false` (default: `false`). And must be relative from the base of the project folder (where node_modules) folder is present.
+* If absolute (static) path to the static assets folder is supplied, then the flag `isPathStatic` must be `true`.
+* The static path option is not fully tested for environments where node_modules is kept shared or not installed with the files, please use with caution, or use absoulte path option for such environments.
+
+## How to use
 
 ```javascript
 import express from 'express';
 import http2 from 'http2';
 
-import expressAutoPush from './index';
+import expressAutoPush from 'express-auto-push';
 
 const app = express();
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as http from 'http';
 import * as http2 from 'http2';
+import * as path from 'path';
 import { Request, Response, NextFunction } from 'express';
 import { AssetCacheConfig, AutoPush, PreprocessResult } from 'h2-auto-push';
 
 /**
- * // TODO: Tune these default parameters.
 const DEFAULT_CACHE_CONFIG: AssetCacheConfig = {
   warmupDuration: 500,
   promotionRatio: 0.8,
@@ -17,6 +17,7 @@ export type RawRequest = http.IncomingMessage | http2.Http2ServerRequest;
 export type RawResponse = http.ServerResponse | http2.Http2ServerResponse;
 export interface AutoPushOptions {
     rootDir: string;
+    isPathStatic?: boolean;
     assetCacheConfig?: AssetCacheConfig;
 }
 
@@ -28,11 +29,19 @@ function isHttp2Response(res: RawResponse): res is http2.Http2ServerResponse {
     return !!(res as http2.Http2ServerResponse).stream;
 }
 
+function staticFilePath(filePath: string): string {
+    return path.join(__dirname, '..', '..', '..', filePath);
+}
+
 const CACHE_COOKIE_KEY = '__ap_cache__';
 let autoPush: AutoPush;
 
 export default function (options: AutoPushOptions) {
     return function (req: Request, res: Response, next: NextFunction): void {
+
+        if (!options.isPathStatic) {
+            options.rootDir = staticFilePath(options.rootDir);
+        }
 
         if (options.assetCacheConfig) {
             autoPush = new AutoPush(options.rootDir, options.assetCacheConfig);    
@@ -48,13 +57,9 @@ export default function (options: AutoPushOptions) {
                 return result.pushFn();
             }).then(next).catch((error: Error) => next(error));
 
-            // req.stream.on('error', (error) => {
-            
-            // });
         }
 
         if (isHttp2Response(res)) {
-            console.log(res.statusCode);
             if (res.statusCode === 404 && req.url) {
                 autoPush.recordRequestPath(res.stream.session, options.rootDir, false);
             } else if (req.statusCode && req.statusCode < 300 && req.statusCode >= 200 && req.url) {
@@ -63,5 +68,3 @@ export default function (options: AutoPushOptions) {
         }
     };
 }
-
-// use express cookieparser missleware before using this middleware
